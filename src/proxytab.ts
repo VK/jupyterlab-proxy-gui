40ebeafc5f7d1afa10f0ebf3d426d3928e431307
VK <viktor@krueckl.de>
2022-05-01 22:26:47 +0200

    lint

--- src/proxytab.ts
+++ src/proxytab.ts
@@ -34,7 +34,7 @@ export class ProxyTab extends IFrame {
     const iframeElement = this.node.children[0] as HTMLHtmlElement;
 
     //some pages don't want to use 100% of the height :)
-    iframeElement.onload = function (): void {
+    iframeElement.onload = function(): void {
       iframeElement.style.height = '100%';
       setTimeout(() => {
         iframeElement.style.height = '100%';
